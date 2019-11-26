dc288c6e22ee6580e4db5a4421b7f72a352bce48
stage <stage@domaine-nt.local>
2019-11-26 16:59:45 +0100

    add setup file, recode all animations, add some elements

--- script.js
+++ script.js
@@ -1,200 +1,13 @@
 
-var current = 'null'
-var frames = {}
+var current = 'null';
 
-$( () => {
+$(() => {
 
-    //===============================================
-    //                                               
-    //    ###    ##     ##  ##  ###    ###  #####  
-    //   ## ##   ####   ##  ##  ## #  # ##  ##     
-    //  ##   ##  ##  ## ##  ##  ##  ##  ##  #####  
-    //  #######  ##    ###  ##  ##      ##  ##     
-    //  ##   ##  ##     ##  ##  ##      ##  #####  
-    //                                               
-    //===============================================
-
-    // Buttons
+    init(true)
 
     $('button').click(()=>{
         hideButton()
     })
-
-    // Bottom
-
-    const BOTTOM = {
-        footer : { top : 90, rotate : -10 },
-        skills : { top : 60, rotate : 15 },
-        projects : { top : 40, rotate : -15 }
-    }
-
-    var delay = 200
-
-    for(classe in BOTTOM){
-        
-        delay += 200
-        const animation = BOTTOM[classe]
-
-        frames[classe] = {
-            initial : [
-                {
-                    targets : 'div.' + classe,
-                    top : animation.top + 'vh',
-                    rotateZ : animation.rotate + 'deg',
-                    easing : 'easeInOutBack',
-                    delay : delay
-                },
-                {
-                    targets : 'p.' + classe,
-                    top : (animation.top + 2) + 'vh',
-                    easing : 'easeOutBack',
-                    delay : delay + 200
-                }
-            ],
-            detail : [
-
-            ]
-        }
-
-        anime(frames[classe].initial[0])
-        anime(frames[classe].initial[1])
-    }
-
-    // HEADER
-
-    frames.header = {
-        initial : [
-            {
-                targets : 'div.header',
-                top : '-90vh',
-                delay : 50
-            },
-            {
-                targets : 'p.header',
-                top : '3vh',
-                delay : 100
-            }
-        ],
-        detail : [
-            
-        ]
-    }
-
-    anime(frames.header.initial[0])
-    anime(frames.header.initial[1])
-
-    // INFOS
-
-    frames.infos = {
-        initial : [
-            {
-                targets : '.infos',
-                top: '18vh',
-                zIndex: {
-                    value: 1,
-                    round: true
-                },
-                duration : 1000
-            },
-            {
-                targets : 'div.infos',
-                height: '10vh',
-                width: '70vw',
-                left : '50vw',
-                duration : 1000
-            },
-            {
-                targets : 'div.infos img',
-                rotateZ : '360deg',
-                easing : 'easeInOutBack',
-                duration : 1000
-            },
-            {
-                targets : 'p.infos',
-                left : '60vw',
-                fontSize: '14px',
-                height: '8vh',
-                width: '40vw',
-                delay : 100,
-                duration : 1000
-            }
-        ],
-        detail : [
-            {
-                targets : '.infos',
-                fontSize: '18px',
-                left : '52vw',
-                top : '55vh',
-                width : '80vw',
-                height: '80vh',
-                zIndex: {
-                    value: 10,
-                    round: true
-                },
-                duration: 1000
-            },
-            {
-                targets : 'p.infos',
-                left: '75vw',
-                duration: 1000
-            }
-        ]
-    }
-
-    setTimeout(()=>{
-        for(const anim of frames.infos.initial){
-            anime(anim)
-        }
-    },1500)
-
-    // CONTACT
-
-    frames.contact = {
-        initial : [
-            {
-                targets : '.contact',
-                left : '40vw',
-                zIndex: {
-                    value: 1,
-                    round: true
-                }
-            },
-            {
-                targets : 'div.contact',
-                top : '35vh',
-                height: '10vh',
-                width: '60vw',
-            },
-            {
-                targets : 'p.contact',
-                fontSize: '14px',
-                top : '35vh',
-                height: '8vh',
-                width: '55vw',
-                delay : 100
-            }
-        ],
-        detail : [
-            {
-                targets : '.contact',
-                fontSize: '20px',
-                left : '52vw',
-                top : '55vh',
-                width : '80vw',
-                height: '80vh',
-                zIndex: {
-                    value: 10,
-                    round: true
-                },
-                duration: 1000
-            }
-        ]
-    }
-
-    setTimeout(()=>{
-        anime(frames.contact.initial[0])
-        anime(frames.contact.initial[1])
-    },1500)
 
     // INDEXES
 
@@ -214,62 +27,43 @@ $( () => {
 
     // onClick listeners
 
-    frames.footer.onClick = function(){
-        if(current == 'null'){
-            for(const anim of frames.footer.detail) anime(anim)
-            showButton('footer')
+    for(classe in animes){
+        const element = animes[classe]
+        if(element.focus){
+            $('.'+classe).click(eval(`()=>{
+                if(current == 'null'){
+                    animes.${classe}.focus()
+                    showButton('${classe}')
+                }
+            }`))
         }
     }
-    frames.skills.onClick = function(){
-        if(current == 'null'){
-            for(const anim of frames.skills.detail) anime(anim)
-            showButton('skills')
-        }
-    }
-    frames.projects.onClick = function(){
-        if(current == 'null'){
-            for(const anim of frames.projects.detail) anime(anim)
-            showButton('projects')
-        }
-    }
-    frames.infos.onClick = function(){
-        if(current == 'null'){
-            for(const anim of frames.infos.detail) anime(anim)
-            showButton('infos')
-        }
-    }
-    frames.contact.onClick = function(){
-        if(current == 'null'){
-            for(const anim of frames.contact.detail) anime(anim)
-            showButton('contact')
-        }
-    }
-    $('.footer').click(frames.footer.onClick)
-    $('.skills').click(frames.skills.onClick)
-    $('.projects').click(frames.projects.onClick)
-    $('.infos').click(frames.infos.onClick)
-    $('.contact').click(frames.contact.onClick)
 
-    //=======================================================================
-    //                                                                       
-    //  #####     ###    #####    ######  ##   ####  ##      #####   ####  
-    //  ##  ##   ## ##   ##  ##     ##    ##  ##     ##      ##     ##     
-    //  #####   ##   ##  #####      ##    ##  ##     ##      #####   ###   
-    //  ##      #######  ##  ##     ##    ##  ##     ##      ##        ##  
-    //  ##      ##   ##  ##   ##    ##    ##   ####  ######  #####  ####   
-    //                                                                       
-    //=======================================================================
+    // hand animation
 
-    
+    anime({
+        targets : '.hand',
+        keyframes : [
+            {
+                left: '55vw',
+                top: '15vh'
+            }
+        ]
+    })
 
 })
+
+function init(starting){
+    for(classe in animes){
+        animes[classe].initial(starting)
+    }
+}
 
 function showButton( name ){
     current = name
     anime({
         targets : 'button',
-        width: '10vw',
-        height: '10vw',
+        left : '90vw',
         begin: ()=>{
             if(current != 'null')
             $('button').show()
@@ -280,15 +74,12 @@ function showButton( name ){
 
 function hideButton(){
     if(current != 'null'){
-        for(const anim of frames[current].initial){
-            anime(anim)
-        }
+        init()
     }
     current = 'null'
     anime({
         targets : 'button',
-        width: '0',
-        height: '0',
+        left : '115vw',
         complete: ()=>{
             if(current == 'null')
             $('button').hide()
---
+++ setup.js
@@ -0,0 +1,299 @@
+
+const animes = {};
+
+$(()=>{
+
+    const width = $(window).width()
+
+    // WARN
+    if(width >= 576){
+        $('body').append('<h1> <i class="fas fa-exclamation-circle"></i> Le responsive du site n\'est disponible pour le moment que sur les petits écrans ! (moins de 576 pixels de large)</h1>').css({
+            position: 'absolute',
+            top: '50vh',
+            left: '50vw',
+            width: '80vw',
+            'z-index': 100,
+            'font-size': '30px',
+            transform: 'translate(-50%,-50%)'
+        })
+    }
+
+    if(width < 576){
+
+//====================================================
+//                                                    
+//  ###    ###   #####   #####   ##  ##      #####  
+//  ## #  # ##  ##   ##  ##  ##  ##  ##      ##     
+//  ##  ##  ##  ##   ##  #####   ##  ##      #####  
+//  ##      ##  ##   ##  ##  ##  ##  ##      ##     
+//  ##      ##   #####   #####   ##  ######  #####  
+//                                                    
+//====================================================
+
+        animes.header = {
+            initial : ()=>{
+                anime({
+                    targets : 'div.header',
+                    top : '-90vh'
+                })
+                anime({
+                    targets : 'section.header',
+                    top : '3vh',
+                    delay : 50
+                })
+            }
+        }
+
+        animes.infos = {
+            initial : ()=>{
+                anime({
+                    targets : '.infos',
+                    top: '18vh',
+                    zIndex: {
+                        value: 1,
+                        round: true
+                    },
+                    duration : 1000
+                })
+                anime({
+                    targets : 'div.infos',
+                    height: '10vh',
+                    width: '70vw',
+                    left : '50vw',
+                    duration : 1000
+                })
+                anime({
+                    targets : 'div.infos img',
+                    rotateZ : '360deg',
+                    easing : 'easeInOutBack',
+                    duration : 1000
+                })
+                anime({
+                    targets : 'section.infos',
+                    left : '60vw',
+                    fontSize: '14px',
+                    height: '8vh',
+                    width: '40vw',
+                    delay : 100,
+                    duration : 1000
+                })
+            },
+            focus : ()=>{
+                anime({
+                    targets : '.infos',
+                    fontSize: '18px',
+                    left : '52vw',
+                    top : '55vh',
+                    width : '80vw',
+                    height: '80vh',
+                    zIndex: {
+                        value: 10,
+                        round: true
+                    },
+                    duration: 1000
+                })
+                anime({
+                    targets : 'section.infos',
+                    left: '75vw',
+                    duration: 1000
+                })
+            }
+        }
+
+        animes.contact = {
+            initial : ()=>{
+                anime({
+                    targets : '.contact',
+                    left : '40vw',
+                    top : '32vh',
+                    zIndex: {
+                        value: 1,
+                        round: true
+                    }
+                })
+                anime({
+                    targets : 'div.contact',
+                    height: '12vh',
+                    width: '60vw',
+                })
+                anime({
+                    targets : 'section.contact',
+                    padding: '0px',
+                    fontSize: '9px',
+                    height: '8vh',
+                    width: '55vw',
+                    delay : 100
+                })
+                anime({
+                    targets : '.contact .icon',
+                    fontSize: '10px',
+                    minWidth: '5vw'
+                })
+                anime({
+                    targets : '.contact .info',
+                    padding: '.5vw 0'
+                })
+            },
+            focus : ()=>{
+                anime({
+                    padding: '15px',
+                    targets : '.contact',
+                    fontSize: '18px',
+                    left : '52vw',
+                    top : '55vh',
+                    width : '80vw',
+                    height: '80vh',
+                    zIndex: {
+                        value: 10,
+                        round: true
+                    },
+                    duration: 1000
+                })
+                anime({
+                    targets : '.contact .icon',
+                    fontSize: '20px',
+                    minWidth: '10vw'
+                })
+                anime({
+                    targets : '.contact .info',
+                    padding: '2.5vw'
+                })
+            }
+        }
+
+        const toBottom = {
+            footer : { top : 90, rotate : -10 },
+            skills : { top : 60, rotate : 15 },
+            projects : { top : 40, rotate : -15 },
+        }
+
+        var delay = 200
+
+        for(classe in toBottom){
+        
+            delay += 200
+            const params = toBottom[classe]
+    
+            animes[classe] = {
+                initial : (starting) => {
+                    anime({
+                        targets : 'div.' + classe,
+                        top : params.top + 'vh',
+                        rotateZ : params.rotate + 'deg',
+                        easing : 'easeInOutBack',
+                    })
+                    anime({
+                        targets : 'section.' + classe,
+                        top : (params.top + 2) + 'vh',
+                        easing : 'easeInOutBack',
+                    })
+                }
+            }
+        }
+
+        const toBottomEntries = Object.entries(toBottom).reverse()
+
+        toBottomEntries.forEach( (entry,index) => {
+
+            const [classe,params] = entry
+            const dessus = toBottomEntries.slice(0,index)
+            const dessous = toBottomEntries.slice(index+1)
+
+            try {
+                animes[classe].focus = eval(`()=>{
+                    anime({
+                        targets : 'div.${classe}',
+                        top : '20vh',
+                        duration : 1000,
+                    })
+                    anime({
+                        targets : 'section.${classe}',
+                        top : '25vh',
+                        duration : 1000,
+                    })
+                    ${dessus.map(e => {
+                        const [c,p] = e
+                        return `anime({
+                            targets : 'div.${c}',
+                            top : '10vh',
+                            duration : 1000
+                        })
+                        anime({
+                            targets : 'section.${c}',
+                            top : '100vh',
+                            duration : 1000
+                        })`
+                    }).join('\n')}
+                    ${dessous.map(e => {
+                        const [c,p] = e
+                        return `anime({
+                            targets : 'div.${c}',
+                            top : '100vh',
+                            duration : 1000
+                        })
+                        anime({
+                            targets : 'section.${c}',
+                            top : '110vh',
+                            duration : 1000
+                        })`
+                    }).join('\n')}
+                }`)
+            } catch (error) {
+                console.error(error)
+            }
+        })
+
+    }else if(width < 768){
+
+//================================================
+//                                                
+//   ####  ###    ###    ###    ##      ##      
+//  ##     ## #  # ##   ## ##   ##      ##      
+//   ###   ##  ##  ##  ##   ##  ##      ##      
+//     ##  ##      ##  #######  ##      ##      
+//  ####   ##      ##  ##   ##  ######  ######  
+//                                                
+//================================================
+
+
+    }else if(width < 992){
+
+//====================================================
+//                                                    
+//  ######    ###    #####   ##      #####  ######  
+//    ##     ## ##   ##  ##  ##      ##       ##    
+//    ##    ##   ##  #####   ##      #####    ##    
+//    ##    #######  ##  ##  ##      ##       ##    
+//    ##    ##   ##  #####   ######  #####    ##    
+//                                                    
+//====================================================
+
+
+    }else if(width < 1200){
+
+//==============================================
+//                                              
+//  ##        ###    #####     ####    #####  
+//  ##       ## ##   ##  ##   ##       ##     
+//  ##      ##   ##  #####    ##  ###  #####  
+//  ##      #######  ##  ##   ##   ##  ##     
+//  ######  ##   ##  ##   ##   ####    #####  
+//                                              
+//==============================================
+
+
+    }else{
+
+//======================
+//                      
+//  ##    ##  ##      
+//   ##  ##   ##      
+//    ####    ##      
+//   ##  ##   ##      
+//  ##    ##  ######  
+//                      
+//======================
+
+
+    }
+})
