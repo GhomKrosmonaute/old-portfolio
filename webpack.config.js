454bd1e3234586d3e2bf3d78e403d224cfe5d2f0
CamilleAbella <camille.abella@hotmail.fr>
2020-07-26 14:25:18 +0200

    try again

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,87 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const SRC = path.resolve( __dirname, 'src' );
-const DIST = path.resolve( __dirname, 'dist' );
+const SRC = path.resolve(__dirname, "src");
+const DIST = path.resolve(__dirname, "dist");
 
 module.exports = {
   context: SRC,
-  
+
   entry: {
-    'main': 'index.ts'
+    main: "./index.ts",
   },
-  
+
   output: {
-    filename: 'index.min.js',
-    path: DIST
+    filename: "index.min.js",
+    path: DIST,
   },
-  
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
-      favicon: "favicon.png"
+      filename: "index.html",
+      template: path.resolve(SRC, "index.html"),
+      favicon: "favicon.png",
     }),
     new CopyWebpackPlugin({
-      patterns: [{
-        from: path.resolve( SRC, "images" ),
-        to: path.resolve( DIST, "images" )
-      }]
-    })
+      patterns: [
+        {
+          from: path.resolve(SRC, "images"),
+          to: path.resolve(DIST, "images"),
+        },
+      ],
+    }),
   ],
-  
+
   resolve: {
-    extensions: ['.ts', '.js'],
-    modules: ['node_modules']
+    extensions: [".ts", ".js"],
+    modules: ["node_modules"],
   },
-  
+
   module: {
     rules: [
       /****************
        * PRE-LOADERS
        *****************/
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'source-map-loader'
+        use: "source-map-loader",
       },
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: 'tslint-loader'
+        use: "tslint-loader",
       },
-      
+
       /****************
        * LOADERS
        *****************/
       {
         test: /\.ts$/i,
         exclude: /node_modules/,
-        use: 'awesome-typescript-loader'
+        use: "awesome-typescript-loader",
       },
       {
         test: /\.s[ac]ss$/i,
         exclude: /node_modules/,
-        use: ['style-loader','css-loader','sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(?:png|svg|jpg|ttf|gif)$/i,
         exclude: /node_modules/,
-        use: 'file-loader'
+        use: "file-loader",
       },
       {
         test: /\.html$/i,
         exclude: /node_modules/,
-        use: 'html-loader'
-      }
-    ]
+        use: "html-loader",
+      },
+    ],
   },
-  
-  devtool: 'cheap-module-source-map',
-  devServer: {}
+
+  devtool: "cheap-module-source-map",
+  devServer: {},
 };
