35fb8add745e6e9b02243d06191d967e3d9943b0
CamilleAbella <camille.abella@hotmail.fr>
2020-07-26 18:30:32 +0200

    start to make json and d.ts data

---
+++ src/data/data.d.ts
@@ -0,0 +1,41 @@
+
+export enum Color {
+  Purple = 0xA100FF,
+  Pink = 0xFF00FF,
+  Magenta = 0xFF0083,
+  Red = 0xFF0010,
+  Orange = 0xFF6E00,
+  Yellow = 0xFFCC00,
+  GreenApple = 0xC3FF00,
+  Green = 0x54FF00,
+  Turquoise = 0x00FFA5,
+  BlueSky = 0x00FAFF,
+  Blue = 0x00AEFF,
+  BlueDark = 0x00AEFF
+}
+
+export enum ProjectType {
+  Org = Color.Purple,
+  Bot = Color.Pink,
+  App = Color.Magenta,
+  Lib = Color.Red,
+  Game = Color.Orange,
+  Site = Color.Yellow,
+  API = Color.GreenApple
+}
+
+export enum SkillType {
+  Lang = Color.Purple,
+  DB = Color.Pink,
+  FW = Color.Magenta,
+  Lib = Color.Red,
+  Server = Color.Orange
+}
+
+export enum ParcoursType {
+  Internship = Color.Purple,
+  Training = Color.Pink,
+  Schooling = Color.Magenta,
+  Working = Color.Red,
+  Sabbath = Color.Orange
+}
